00d35a559ea8de310a1ad2399a282881196e4fd8
Droogans <droogans@gmail.com>
2014-07-05 11:45:50 -0500

    feat(search): Filter by tags.

--- search/search.js
+++ search/search.js
@@ -3,14 +3,29 @@ angular.module('searchApp', [], function ($interpolateProvider) {
     $interpolateProvider.endSymbol('}]}');
 })
 .filter('matchesQuery', function() {
-    return function(items, query){
+    return function (items, query){
         var alternate = query.replace(/ /g,"_").toLowerCase();
         var lcQuery = query.toLowerCase();
         var matches = [];
-        for (var i=0; i < items.length; i++){
+        for (var i=0; i < items.length; i++) {
             var matchesStrictly = items[i].title.toLowerCase().indexOf(lcQuery) !== -1;
             var matchesLoosely = items[i].content.indexOf(alternate) !== -1;
             if (matchesStrictly || matchesLoosely) {
+                matches.push(items[i]);
+            }
+        }
+        return matches;
+    };
+})
+.filter('matchesTag', function () {
+    return function (items, tag) {
+        if (tag === undefined) {
+            return items;
+        }
+
+        var matches = [];
+        for (var i=0; i < items.length; i++) {
+            if (items[i].tags.indexOf(tag) > -1) {
                 matches.push(items[i]);
             }
         }
@@ -21,7 +36,12 @@ angular.module('searchApp', [], function ($interpolateProvider) {
 function PostListCtrl($scope, $http) {
     $scope.query = "";
     $scope.posts = [];
+    $scope.tag = undefined;
     $http.get('/search/feeds.json').success(function(data) {
         $scope.posts = data;
     });
+
+    $scope.addTag = function (tag) {
+        $scope.tag = $scope.tag === tag ? undefined : tag;
+    };
 }
