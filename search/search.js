e6947a677aa2185bf784ef5c82b6f828fda5acca
Droogans <droogans@gmail.com>
2019-12-17 02:24:47 +0100

    refact(search): Mostly working

--- search/search.js
+++ search/search.js
@@ -1,47 +1,39 @@
-function matchingTags(items, tag) {
-    if (tag === undefined) {
-        return items;
-    }
-
-    var matching = [];
-    for (var i=0; i < items.length; i++) {
-        if (items[i].tags.indexOf(tag) > -1) {
-            matching.push(items[i]);
+function filterPostsByTag(tag) {
+    // TODO:
+    // active tag
+    // add multi
+    // toggle
+    for (post of matchingPosts.getElementsByTagName("li")) {
+        if (post.getAttribute("data-tags").indexOf(tag) == -1) {
+            post.classList.add("hidden-by-tag");
+        } else {
+            post.classList.remove("hidden-by-tag");
         }
     }
-    return matching;
 }
 
-function matchingQueries(items, query) {
-    var alternate = query.replace(/ /g,"_").toLowerCase();
-    var lcQuery = query.toLowerCase();
-    var matching = [];
-    for (var i=0; i < items.length; i++) {
-        var matchingStrictly = items[i].title.toLowerCase().indexOf(lcQuery) !== -1;
-        var matchingLoosely = items[i].content.indexOf(alternate) !== -1;
-        if (matchingStrictly || matchingLoosely) {
-            matching.push(items[i]);
+function filterPostsByQuery(event) {
+    const q = query.value.toLowerCase();
+    const alt = q.replace(/ /g,"_");
+    const matchingPosts = window.document.getElementById("matchingPosts");
+    for (post of matchingPosts.getElementsByTagName("li")) {
+        const link = post.firstElementChild.pathname;
+        const item = window.posts[link];
+        const title = item.title.toLowerCase();
+        if (q && (title.indexOf(q) === -1 || (item.excerpt && item.excerpt.indexOf(alt) === -1))) {
+            post.classList.add("hidden-by-query");
+        } else {
+            post.classList.remove("hidden-by-query");
         }
     }
-    return matching;
-}
-
-function redrawPostsList(e) {
-    // TODO, just use vanilla js and don't worry about heavy repaints with tags/search text overlapping
-}
-
-function addTagToSearchPath(tag) {
-    const urlParams = new URLSearchParams(window.location);
-    const tags = urlParams.get('tags') || [];
-    const uniqueTags = new Set([...tags, tag]);
-    urlParams.set('tags', [...uniqueTags]);
-    debugger;
-    location.search = urlParams.getAll('tags').toString();
 }
 
 window.posts = {};
-fetch('/search/feeds.json').then(response => {
+fetch("/search/feeds.json").then(response => {
     response.json().then(posts => {
-        window.posts = posts;
+        for (post of posts) {
+            const link = new URL(post.link).pathname;
+            window.posts[link] = post;
+        }
     });
 });
