2a61c0966073ebf2cbb9e7ff83c3f5e36371517b
Droogans <droogans@gmail.com>
2019-12-17 09:20:43 +0100

    refact(search): Drop angular
    Closes #55
    Closes #52

--- search/search.js
+++ search/search.js
@@ -1,14 +1,35 @@
-function filterPostsByTag(tag) {
-    // TODO:
+function filterPostsByTag(event, tag) {
     // active tag
-    // add multi
-    // toggle
+    const classList = event.target.classList;
+    if (classList.contains("filtered-tag")) {
+        classList.remove("filtered-tag");
+    } else {
+        classList.add("filtered-tag");
+    }
+
+    const tagList = window.document.getElementById("tagList");
+    const activeTags = tagList.getElementsByClassName("filtered-tag");
+    const tags = [];
+    for (activeTag of activeTags) {
+        tags.push(activeTag.innerText);
+    }
+
     for (post of matchingPosts.getElementsByTagName("li")) {
-        if (post.getAttribute("data-tags").indexOf(tag) == -1) {
-            post.classList.add("hidden-by-tag");
-        } else {
+        if (tags.length === 0) {
             post.classList.remove("hidden-by-tag");
+            continue;
         }
+
+        let allMatches = true;
+        for (tag of tags) {
+            const postTags = post.getAttribute("data-tags").split(" ");
+            if (postTags.indexOf(tag) === -1) {
+                post.classList.add("hidden-by-tag");
+                allMatches = false;
+                break;
+            }
+        }
+        allMatches && post.classList.remove("hidden-by-tag");
     }
 }
 
